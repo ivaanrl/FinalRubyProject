bde7c56c55031b257ee8df5a0985a0098b985d06
ivaanrl <ivanrl2010@gmail.com>
2019-08-31 20:34:08 -0300

    Add knight movement.

--- knight.rb
+++ knight.rb
@@ -3,6 +3,21 @@ require_relative 'piece'
 class Knight < Piece
   attr_accessor :piece
   def initialize(color)
-    color =='white' ? @piece = "\u2658 " : @piece = "\u265D "
+    color =='white' ? @piece = "\u2658 " : @piece = "\u265E "
+  end
+
+  def self.move(initial_square, target_square, taken, turn)
+    if turn != taken[1]
+      if (initial_square[0] + 2 == target_square[0] && (initial_square[1] + 1 == target_square[1] || initial_square[1] - 1 == target_square[1])) ||
+         (initial_square[0] - 2 == target_square[0] && (initial_square[1] + 1 == target_square[1] || initial_square[1] - 1 == target_square[1])) ||
+         (initial_square[1] + 2 == target_square[1] && (initial_square[0] + 1 == target_square[0] || initial_square[0] - 1 == target_square[0])) ||
+         (initial_square[1] - 2 == target_square[1] && (initial_square[0] + 1 == target_square[0] || initial_square[0] - 1 == target_square[0]))
+          return true 
+      else  
+        puts "That's not a valid move. Knights can only move in L shape."
+      end
+    elsif turn == taken[0]
+      puts "There's another #{turn} piece there. Please choose your coordinates again."
+    end
   end
 end
