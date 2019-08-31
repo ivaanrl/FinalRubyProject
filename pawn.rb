eadcc9bc861397931386ab381c6344ecaaacd41d
ivaanrl <ivanrl2010@gmail.com>
2019-08-31 20:34:35 -0300

    Fix some movement issues. Pawn can eat other pieces now.

--- pawn.rb
+++ pawn.rb
@@ -3,20 +3,30 @@ require_relative 'piece'
 class Pawn < Piece
   attr_accessor :piece
   def initialize(color)
-    @move_count = 0
     color == 'white' ? @piece = "\u2659 " : @piece = "\u265F "
   end
 
-  def self.move(initial_square, target_square)
-    if @move_count == 0
-      if initial_square[0] + 1 == target_square[0] || initial_square[0] + 2 == target_square[0]
-        @move_count += 1
-        return true 
+  def self.move(initial_square, target_square, taken, turn)
+    if taken[0] 
+      if turn != taken[1]
+        if initial_square[0] + 1 == target_square[0] && initial_square[1] + 1 == target_square[1]
+          return true
+        else
+          puts "There's another piece there. Pawns can only eat in diagonal."
+        end
+      else
+        puts "There's another #{turn} piece there. Choose another move"
       end
-    elsif initial_square[0] + 1 == target_square[0]
-        return true
-    else 
-      puts "That's not a valid Pawn move."
+    else
+      if (initial_square[0] == 2 && turn == 'white') || (initial_square[0] == 7 && turn == 'black')
+        if initial_square[0] + 1 == target_square[0] || initial_square[0] + 2 == target_square[0]
+          return true 
+        end
+      elsif initial_square[0] + 1 == target_square[0]
+          return true
+      else 
+        puts "That's not a valid Pawn move. Choose your coordinates again."
+      end
     end
   end 
 end
